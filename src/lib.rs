6f5bc9737956f37989f588782a2a3507ab144332
Andrew S <andrews3596@gmail.com>
2021-07-19 23:32:47 -0400

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,14 @@ extern crate parasailors;
 extern crate needletail;
 extern crate serde_json;
 extern crate itertools;
+extern crate regex;
+use regex::Regex;
 use std::path::Path;
 use std::fs;
 use std::collections::HashMap;
 use std::collections::VecDeque;
 use parasailors::*;
+use std::cmp;
 
 // Will use traits later
 pub struct InputSequence<'a> {
@@ -21,13 +24,14 @@ pub struct InputSequence<'a> {
 }
 
 
-pub fn cluster(sorted_data: &Vec<&InputSequence>) {
+pub fn cluster<'a>(sorted_data: &'a Vec<InputSequence<'a>>) -> (HashMap<usize, std::vec::Vec<&InputSequence<'a>>>, HashMap<usize, InputSequence<'a>>, HashMap<Vec<u8>, std::vec::Vec<usize>>) {
     // Arguments. Need to move out.
     let args_k = 1;
     let args_w = 1;
     let args_min_shared = 1;
     let args_min_shared = 1;
     let args_min_fraction = 1;
+    let args_aligned_threshold = 1.0;
     let args_min_prob_no_hits = 1.0;
     let args_mapped_threshold = 1.0;
 
@@ -67,40 +71,23 @@ pub fn cluster(sorted_data: &Vec<&InputSequence>) {
         representatives.insert(sequence.read_cl_id, new);
     }
 
+    let mut cluster_to_new_cluster_id = HashMap::new();
+
     for sequence in sorted_data {
 
         // Step 1.1: Compress reads by removing consecutive duplicates
        // seq_hpol_comp = ''.join(ch for ch, _ in itertools.groupby(seq))
        
         //seq_hpol_comp.dedup();
-        let mut all_read_hpol_lengths = Vec::new();
-        let mut current_count = 0;
-        let mut last_val: u8 = 0; // TODO, check if safe.
-        let seq_hpol_comp: Vec<u8> = sequence.sequence.iter().filter(
-            |val| {
-                let is_duplicate = last_val == **val;
-                last_val = **val;
-                if is_duplicate {
-                    current_count += 1;
-                } else {
-                    all_read_hpol_lengths.push(current_count);
-                    current_count = 1;
-                }
-                return !is_duplicate;
-            }
-        ).cloned().collect();
-        all_read_hpol_lengths.push(current_count);
-       
         
-
+        let (seq_hpol_comp, all_read_hpol_lengths) = get_seq_hpol_comp(sequence.sequence);
         if seq_hpol_comp.len() < args_k as usize {
             print!( "skipping read of length: {} homopolymer compressed: {} sequence: {}", sequence.sequence.len(), seq_hpol_comp.len(), String::from_utf8(sequence.sequence.to_vec()).unwrap());
             continue;
         }
 
         // Step 1.2: Get minimizers
-        let minimizers = get_kmer_minimizers(&seq_hpol_comp,args_k, args_w);
-
+        let minimizers = get_kmer_minimizers(&seq_hpol_comp, args_k, args_w);
         // 2. Find the homopolymer compressed error rate (else statement is the only one active in single core mode)
        // if representatives.get(sequence.read_cl_id).unwrap().len() == 7 { // we have already computed homopolymenr compressed error rate in previous iteration (if isONclust is called with multiple cores):
             // not implemented as single-core only for now.
@@ -137,26 +124,74 @@ pub fn cluster(sorted_data: &Vec<&InputSequence>) {
         // 5. If step 4 is unsuccessfull we try to align the read to the representative(s) with the most shared minimizers.
 
 
-        if best_cluster_id_m.is_none() && nr_shared_kmers_m >= args_min_shared {
+        let mut best_cluster_id_a = -1;
+
+        if best_cluster_id_m < 0 && nr_shared_kmers_m >= args_min_shared {
             // aln_called += 1
-            let (best_cluster_id_a, nr_shared_kmers_a, error_rate_sum, s1_alignment, s2_alignment, alignment_ratio) = get_best_cluster_block_align(sequence.read_cl_id, &representatives, &hit_clusters_ids, &hit_clusters_hit_positions);
-            if best_cluster_id_a >= 0 {
-              //  aln_passed_criteria += 1
-            } else {
-                best_cluster_id_a = -1
-            }
+            let (best_cluster_id_a, nr_shared_kmers_a, error_rate_sum, s1_alignment, s2_alignment, alignment_ratio) = get_best_cluster_block_align(sequence.read_cl_id, &representatives, &hit_clusters_ids, &hit_clusters_hit_positions, args_k, args_aligned_threshold);
         }
         
         // 6. Adds current read to representative, or makes it a new representative of a new cluster.
        
+        let best_cluster_id = cmp::max(best_cluster_id_m, best_cluster_id_a);
+       
+        if best_cluster_id >= 0 {
+            cluster_to_new_cluster_id.insert(sequence.read_cl_id, best_cluster_id as usize);
+        } else {
+            // 7. If new representative: add the minimizers to the minimizer database. 
 
-
-
+            for (k, pos) in minimizers {
+                if minimizer_database.contains_key(k) {
+                    minimizer_database.get_mut(k).unwrap().push(sequence.read_cl_id);
+                } else {
+                    let mut v = Vec::new();
+                    v.push(sequence.read_cl_id);
+                    minimizer_database.insert(k.to_owned(), v);
+                }
+            }
+        }
     }
-
+    for read_cl_id in cluster_to_new_cluster_id.values() {
+        let new_cl_id = cluster_to_new_cluster_id.get(read_cl_id).unwrap();
+        let all_reads = clusters.get(read_cl_id).unwrap().to_owned();
+        let cl = clusters.get_mut(new_cl_id).unwrap();
+        for read_acc in all_reads {
+            cl.push(read_acc);
+        }
+        clusters.remove(read_cl_id);
+        representatives.remove(read_cl_id);
+    }
+   
+ 
+    // Output:
+    return (clusters, representatives, minimizer_database);
 }
 
-fn get_best_cluster_block_align(read_cl_id: usize, representatives: &HashMap<usize, InputSequence>, hit_clusters_ids: &HashMap<usize, u64>, hit_clusters_hit_positions: &HashMap<usize, Vec<usize>>, args_k: i64) {
+fn get_seq_hpol_comp<'a> (sequence: &'a [u8]) -> (Vec<u8>, Vec<usize>) {
+    let mut last_val: u8 = 0; // TODO, check if safe.
+    let mut current_count = 0;
+    let mut all_read_hpol_lengths = Vec::new();
+    let seq_hpol_comp: Vec<_> = sequence.iter().filter(
+        |val| {
+            let is_duplicate = last_val == **val;
+            last_val = **val;
+            if is_duplicate {
+                current_count += 1;
+            } else {
+                all_read_hpol_lengths.push(current_count);
+                current_count = 1;
+            }
+            return !is_duplicate;
+        }
+    ).cloned().collect();
+
+    all_read_hpol_lengths.push(current_count);
+
+    return (seq_hpol_comp, all_read_hpol_lengths);
+}
+
+fn get_best_cluster_block_align(read_cl_id: usize, representatives: &HashMap<usize, InputSequence>, hit_clusters_ids: &HashMap<usize, u64>, hit_clusters_hit_positions: &HashMap<usize, Vec<usize>>, args_k: i64, args_aligned_threshold: f64) 
+-> (i64, usize, f64, Option<Vec<u8>>, Option<Vec<u8>>, f64) {
     let best_cluster_id = -1;
     let mut top_matches: Vec<(&usize, &Vec<usize>)> = hit_clusters_hit_positions.iter().collect();
     top_matches.sort_by_key(|x| (x.1.len(), x.1.iter().sum::<usize>(), representatives.get(x.0).unwrap().acc));
@@ -198,25 +233,113 @@ fn get_best_cluster_block_align(read_cl_id: usize, representatives: &HashMap<usi
             gap_opening_penalty = 2;
         }
 
-        let match_id_tailored = (1.0 - (error_rate_sum) * args_k as f64) as i64;
+        let match_id_tailored = ((1.0 - error_rate_sum) * args_k as f64) as i64;
         
-        let (s1, s2, (s1_alignment, s2_alignment, alignment_ratio)) = parasail_block_alignment(seq, c_seq, args_k, match_id_tailored, gap_opening_penalty);
+        let (s1_alignment, s2_alignment, alignment_ratio) = parasail_block_alignment(seq, c_seq, args_k, match_id_tailored, gap_opening_penalty);
       
-
+        if alignment_ratio > args_aligned_threshold {
+            return (*cl_id as i64, nm_hits, error_rate_sum, Some(s1_alignment), Some(s2_alignment), alignment_ratio);
+        }
     }
+    return (-1, 0, 0.0, None, None, 0.0);
 }
 
-fn parasail_block_alignment(s1: &[u8], s2: &[u8], k: u64, match_id: i64, match_score: i64, mismatch_penalty: i64, opening_penalty: i64, gap_ext: i64) {
+
+fn parasail_block_alignment(s1: &[u8], s2: &[u8], k: i64, match_id: i64, opening_penalty: i32) ->
+    (Vec<u8>, Vec<u8>, f64) {
+
+    let mismatch_penalty = -2;
+    let gap_ext = 1;
+    let match_score = 2;
 
     // def parasail_block_alignment(s1, s2, k, match_id, match_score = 2, mismatch_penalty = -2, opening_penalty = 5, gap_ext = 1):
 
     let user_matrix = Matrix::create("ACGT", match_score, mismatch_penalty);
-    let result = parasailors::
+    let result = parasailors::semi_global_alignment_trace_scan_sat_cigar(s1, s2, opening_penalty, gap_ext, &user_matrix);
+
+    let (s1_alignment, s2_alignment) = cigar_to_seq(&result.cigar_trace, s1, s2);
+
+    let match_vector: Vec<i64> = s1_alignment.iter().zip(s2_alignment.iter()).map(|(n1,n2)| if n1 == n2 { return 1; } else { return 0; }).collect();
+    let mut match_window: VecDeque<i64> = match_vector[0..k as usize].iter().cloned().collect();
+    let mut current_match_count: i64 = match_window.iter().sum();
+
+    let mut aligned_region = Vec::new();
+    if current_match_count >= match_id {
+        aligned_region.push(1);
+    } else {
+        aligned_region.push(0);
+    }
+
+    for new_m_state in match_vector[k as usize..].iter() {
+        let prev_m_state = match_window.pop_front().unwrap();
+        current_match_count = current_match_count - prev_m_state + new_m_state;
+
+        match_window.push_back(*new_m_state);
+
+        if current_match_count >= match_id {
+            aligned_region.push(1);
+        } else {
+            aligned_region.push(0);
+        }
+    }
+
+    let alignment_ratio = aligned_region.iter().sum::<i64>() as f64 / s1.len() as f64;
+
+    return (s1_alignment, s2_alignment, alignment_ratio);
 
 }
 
+fn cigar_to_seq(cigar: &String, query: &[u8], refs: &[u8]) -> (Vec<u8>, Vec<u8>) {
+
+    let mut cigar_tuples = Vec::new();
+    let re = Regex::new(r"[=DXSMI]+").unwrap();
+    let result: Vec<&str> = re.split(cigar).collect();
+    let mut i = 0;
+
+    for index in 0..(result.len() - 1) {
+        let length = result[index];
+        i += length.len();
+        let type_ = cigar.as_bytes()[i] as char;
+        i += 1;
+        cigar_tuples.push((length.parse::<usize>().unwrap(), type_));
+    }
+
+    let mut r_index = 0;
+    let mut q_index = 0;
+    let mut q_aln = Vec::new();
+    let mut r_aln = Vec::new();
+
+    for (length_, type_) in cigar_tuples {
+        if type_ == '=' || type_ == 'X' {
+            q_aln.extend_from_slice(&query[q_index..(q_index + length_)]);
+            r_aln.extend_from_slice(&refs[r_index..(r_index + length_)]);
+
+            r_index += length_;
+            q_index += length_;
+        } else if type_ == 'I' {
+            // insertion w.r.t. reference
+            r_aln.extend_from_slice(&std::iter::repeat("-").take(length_).collect::<String>().as_bytes());
+            q_aln.extend_from_slice(&query[q_index..(q_index + length_)]);
+          
+            q_index += length_;
+
+        } else if type_ == 'D' {
+            // deletion w.r.t. reference
+            r_aln.extend_from_slice(&refs[r_index..(r_index + length_)]);
+            q_aln.extend_from_slice(&std::iter::repeat("-").take(length_).collect::<String>().as_bytes());
+   
+            r_index += length_;
+        } else {
+            println!("Error!");
+            println!("{}", cigar);
+            panic!();
+        }
+    }
+    return (q_aln, r_aln);
+}
+
 fn get_best_cluster(read_cl_id: usize, compressed_seq_len: usize, hit_clusters_ids: &HashMap<usize, u64>, hit_clusters_hit_positions: &HashMap<usize, Vec<usize>>, minimizers: &Vec<(&[u8], usize)>, nummber_of_minimizers: usize, hit_clusters_hit_index: &HashMap<usize, Vec<usize>>, representatives: &HashMap<usize, InputSequence>, p_emp_probs: &HashMap<String, f64>, args_min_shared: usize, args_min_fraction: usize, args_min_prob_no_hits: f64, args_mapped_threshold: f64) 
-    -> (Option<usize>, usize, f64) {
+    -> (i64, usize, f64) {
 
     /*
           Tally up total covered (mapped region) and compare it with total unmapped region. What is counted as a consecutive mapped block depends on minimizer qualitity
@@ -225,8 +348,14 @@ fn get_best_cluster(read_cl_id: usize, compressed_seq_len: usize, hit_clusters_i
         Return: An integer >= 0 denoting the cluster ID that this read was assigned to. In not assigend to any previous cluster, return None. 
                 [Also returing mapped ratio and nr shared minimizers to best read for logging purposes.]
     */
-    let mut nr_shared_kmers = 0;
+    
+    let nr_shared_kmers = 0;
     let mut mapped_ratio = 0.0;
+
+    if hit_clusters_ids.len() == 0 {
+        return (-1, nr_shared_kmers, mapped_ratio);
+    }
+
     let mut top_matches: Vec<(&usize, &Vec<usize>)> = hit_clusters_hit_positions.iter().collect();
     top_matches.sort_by_key(|x| (x.1.len(), x.1.iter().sum::<usize>(), representatives.get(x.0).unwrap().acc));
     top_matches.reverse();
@@ -253,9 +382,9 @@ fn get_best_cluster(read_cl_id: usize, compressed_seq_len: usize, hit_clusters_i
 
             let p_error_in_kmers_emp = 1.0 - p_shared_minimizer_empirical(error_rate_read, error_rate_c, p_emp_probs);
             let minimizer_error_probabilities = vec![p_error_in_kmers_emp; nummber_of_minimizers];
-            let total_mapped = 0;
+            let mut total_mapped = 0;
 
-            let prob_all_errors_since_last_hit = Vec::new();
+            let mut prob_all_errors_since_last_hit = Vec::new();
             prob_all_errors_since_last_hit.push(minimizer_error_probabilities[..minimizer_hit_indices[0]].iter().fold(1.0, |a, b| a*b));
             for (hit_idx1, hit_idx2) in minimizer_hit_indices[..minimizer_hit_indices.len() - 1].iter().zip(minimizer_hit_indices[1..].iter()) {
                 prob_all_errors_since_last_hit.push(minimizer_error_probabilities[hit_idx1+1..*hit_idx2].iter().fold(1.0, |a, b| a*b));
@@ -284,7 +413,7 @@ fn get_best_cluster(read_cl_id: usize, compressed_seq_len: usize, hit_clusters_i
             mapped_ratio = total_mapped as f64 / compressed_seq_len as f64;
 
             if mapped_ratio > args_mapped_threshold {
-                return (Some(*cl_id), nm_hits, mapped_ratio);
+                return (*cl_id as i64, nm_hits, mapped_ratio);
             }
             // [reduce(mul, minimizer_error_probabilities[: minimizer_hit_indices[0]], 1)] +  
             // [ reduce(mul, minimizer_error_probabilities[hit_idx1+1: hit_idx2], 1) for hit_idx1, hit_idx2 in zip(minimizer_hit_indices[:-1], minimizer_hit_indices[1:]) ] + 
@@ -293,7 +422,7 @@ fn get_best_cluster(read_cl_id: usize, compressed_seq_len: usize, hit_clusters_i
         }
     }
     
-    return (None, nr_shared_kmers, mapped_ratio);
+    return (-1, nr_shared_kmers, mapped_ratio);
 }
 
 fn p_shared_minimizer_empirical(error_rate_read: f64, error_rate_center: f64, p_emp_probs: &HashMap<String, f64>) -> f64 {
@@ -315,7 +444,7 @@ fn p_shared_minimizer_empirical(error_rate_read: f64, error_rate_center: f64, p_
     return *p_kmer_shared;
 }
 
-fn get_all_hits(minimizers: &Vec<(&[u8], usize)>, minimizer_database: &HashMap<&[u8], Vec<usize>>, read_cl_id: usize) 
+fn get_all_hits(minimizers: &Vec<(&[u8], usize)>, minimizer_database: &HashMap<Vec<u8>, Vec<usize>>, read_cl_id: usize) 
     -> (HashMap<usize, u64>, HashMap<usize, Vec<usize>>,HashMap<usize, Vec<usize>>) {
 
     //     Get all representatives ID's that shares matches with the minimizers in the read.  
@@ -325,8 +454,8 @@ fn get_all_hits(minimizers: &Vec<(&[u8], usize)>, minimizer_database: &HashMap<&
     let mut hit_clusters_hit_positions = HashMap::new();
 
     for (i, (m, pos)) in minimizers.iter().enumerate() {
-        if minimizer_database.contains_key(m) {
-            for cl_id in minimizer_database.get(m).unwrap() {
+        if minimizer_database.contains_key(*m) {
+            for cl_id in minimizer_database.get(*m).unwrap() {
                 hit_clusters_ids.insert(*cl_id, hit_clusters_ids.get(&cl_id).unwrap_or(&0) + 1);
                 hit_clusters_hit_index.entry(*cl_id).or_insert(Vec::new()).push(i);
                 hit_clusters_hit_positions.entry(*cl_id).or_insert(Vec::new()).push(*pos);
